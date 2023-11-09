7aeffc8eeb47f906b1aa09faf2c979219af9da67
mdmuneeb <muhammadmuneebzafar30@gmail.com>
2023-11-09 19:21:32 +0500

    Chapter 43-48

---
+++ app.js
@@ -0,0 +1,17 @@
+function openAlert1()
+{
+    alert("we are moving you to the link website")
+}
+
+
+function openAlert2()
+{
+    alert("this is a samsung mobile")
+}
+
+function Delete(id)
+{
+    let elem = document.getElementById(id);
+    elem.remove()
+}
+
